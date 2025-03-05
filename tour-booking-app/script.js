eea3de0b79f27145f33b1130c631f5147fbe21be
shansswj <shan.swea@proton.me>
2025-03-05 18:07:05 +0800

    Added JavaScript for booking buttons

---
+++ tour-booking-app/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded. function () {
+
+    let buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            console.log("Booking confirmed for: "+ this.parentElement.textContent.trim());
+        }); 
+    });
+});
