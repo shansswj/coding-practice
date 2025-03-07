5fb6af0953f41a44c3a6d4622c5a959b52fc036e
shansswj <shan.swea@proton.me>
2025-03-07 18:34:30 +0800

    cleared mistake

--- tour-booking-app/script.js
+++ tour-booking-app/script.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded. function () {
+document.addEventListener('DOMContentLoaded' , function () {
 
     let buttons = document.querySelectorAll("button");
 
     buttons.forEach(button => {
         button.addEventListener("click", function () {
             console.log("Booking confirmed for: "+ this.parentElement.textContent.trim());
-            boom
         }); 
     });
 });
